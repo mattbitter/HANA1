edbaca2c9348add315081147f25a3998b0d59f5e
mattbitter <bitter.mca@gmail.com>
2017-02-12 02:44:55 -0500

    update

--- TextSR2_testing.sql
+++ TextSR2_testing.sql
@@ -13,14 +13,20 @@ drop type "TT_MT";
 CREATE TYPE TT_MT AS TABLE ("SR" INT, "RANK" INT, "NORMALIZED_TERM" NVARCHAR(100));
 create table "T_MT" like "TT_MT";
 
-			
+DROP PROCEDURE "P_MT";			
 CREATE PROCEDURE "P_MT" (IN    ID2    INTEGER) LANGUAGE SQLSCRIPT AS
 /*********BEGIN PROCEDURE SCRIPT ************/
 BEGIN
-			
+			OID = select * from T_MT;
 	    --insert into "T_MT" --values (RANK, NORMALIZED_TERM)
-		    OID = (:ID2) as ("SR");
-
+		     OID = SELECT T.RANK, T.NORMALIZED_TERM
+		    FROM TM_GET_RELEVANT_TERMS (
+			DOCUMENT IN FULLTEXT INDEX WHERE "pk_ID" = :ID2
+			SEARCH "Description" FROM "SYSTEM"."t_sr" 
+			RETURN TOP 200
+			) AS T;
+			
+			update top 200 :OID SET "SR" = :ID2;
 			
 		insert into "T_MT" select * from :OID;
 		
